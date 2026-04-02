a64d72baff41f6715c6ad1d57c0bb921e39ee9db
Marcus-Mon <marcusrafaelmontilla@gmail.com>
2026-04-02 11:58:30 -0400

    Fixing again

--- HeladeriaProyect/Entities/Usuario.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HeladeriaProyect.Entities
-{
-    public class Usuario
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
