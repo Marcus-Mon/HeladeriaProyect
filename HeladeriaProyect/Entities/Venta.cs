26b5264ab2b9674d328b4cdddc898111808acf4b
Marcus-Mon <marcusrafaelmontilla@gmail.com>
2026-04-01 22:28:27 -0400

    Scrapped other entities

--- HeladeriaProyect/Entities/Venta.cs
+++ HeladeriaProyect/Entities/Venta.cs
@@ -6,8 +6,5 @@
         public DateTime Fecha { get; set; }
         public decimal Total { get; set; }
 
-        public int UsuarioId { get; set; }
-        public Usuario Usuario { get; set; }
-        public List<DetalleVenta> Detalles { get; set; }
     }
 }
