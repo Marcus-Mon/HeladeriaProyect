3a16db368e999033778f49e9747113a316470686
Marcus-Mon <marcusrafaelmontilla@gmail.com>
2026-04-02 14:40:19 -0400

    bruh

--- HeladeriaProyecto/Services/UsuarioService.cs
+++ HeladeriaProyecto/Services/UsuarioService.cs
@@ -1,4 +1,4 @@
-﻿using HeladeriaProyecto.Data;
+﻿using HeladeriaProyect.Data;
 using Microsoft.Data.SqlClient;
 
 namespace HeladeriaProyect.Services
@@ -30,9 +30,6 @@ namespace HeladeriaProyect.Services
                 }
             }
         }
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
     }
 }
