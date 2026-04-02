ce5f659c287e32f9b3da7942862552df09701a07
Marcus-Mon <marcusrafaelmontilla@gmail.com>
2026-04-02 15:39:31 -0400

    Creating SaborService

---
+++ HeladeriaProyecto/Services/SaborService.cs
@@ -0,0 +1,28 @@
+﻿using HeladeriaProyect.Data;
+using HeladeriaProyecto.Entities;
+using Microsoft.Data.SqlClient;
+
+namespace HeladeriaProyecto.Services
+{
+    public class SaborService
+    {
+        private Conexion conexion = new Conexion();
+
+        public void Crear(SaborHelado sabor)
+        {
+            using (SqlConnection conn = conexion.ObtenerConexion())
+            {
+                string query = "INSERT INTO Sabores (Nombre, Descripcion, Precio) VALUES (@n, @d, @p)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@n", sabor.Nombre);
+                cmd.Parameters.AddWithValue("@d", sabor.Descripcion);
+                cmd.Parameters.AddWithValue("@p", sabor.Precio);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
